8acbfb5f1c8361ef306a0a27995120122891fc55
Ibraheem Sheikh <Ibraheemsheikh2@gmail.com>
2024-06-03 01:40:17 +0500

    2 projects in Portfolio Added

--- data/home.js
+++ data/home.js
@@ -1,240 +1,247 @@
 // ? Landing Page
 
 export const hero = {
-	buttonText: "Explore",
-	buttonLink: "#statsSections",
-	image: "/images/pics/ibrahim.png",
+  buttonText: "Explore",
+  buttonLink: "#statsSections",
+  image: "/images/pics/ibrahim.png",
 };
 
 export const services = {
-	title: "Our Services",
-	description: "",
-	services: [
-		{
-			title: "UI UX Design",
-			description:
-				"Transforming digital experiences with  UI/UX for mobile, websites, and web apps. Our expert team delivers pixel-perfect designs and seamless interactions that captivate users. Elevate your brand with our cutting-edge solutions.",
-			image: "/images/icons/logo-and-branding.svg",
-		},
-		{
-			title: "MVP Development",
-			description:
-				"Accelerate your product's journey to success with our MVP (Minimum Viable Product) development services. We specialize in rapidly prototyping and launching your idea, allowing you to test the market.",
-			image: "/images/icons/service4.svg",
-		},
-		{
-			title: "Biz Success Accelerator",
-			description:
-				"Establish Business Channel. Here, we'll create a stunning landing page, implement effective lead generation strategies, setup Google Ads campaigns, integrate Calendly, and establish Solid Base for you! ",
-			image: "/images/icons/service3.svg",
-		},
-		
-		{
-			title: "Website Design and Development",
-			description:
-				"Unlock the full potential of your online business with our top-notch website development services. Our skilled team of developers will bring your vision to life, crafting a scalable website.",
-			image: "/images/icons/service2.svg",
-		},
-		{
-			title: "Mobile app Design and Development",
-			description:
-				"Revolutionize with our mobile app design and development services. Our expert team will create a captivating and user-friendly app, tailored to your goals. Book with us a meeting and let's explore!",
-			image: "/images/icons/service5.svg",
-		},
-		{
-			title: "Web app Design and Development",
-			description:
-				"Unleash your digital potential with our web app design and development services. Our team crafts stunning and functional web applications that elevate your business.",
-			image: "/images/icons/service6.svg",
-		},
-		{
-			title: "Game Development",
-			description:
-				"Level up your gaming experience with our exceptional game development services. Our talented team brings your ideas to life, creating immersive and captivating games that engage players.",
-			image: "/images/icons/service2.svg",
-		},
-		{
-			title: "AR VR Development",
-			description:
-				"Immerse your audience in extraordinary experiences with our AR/VR development services. Our team creates captivating augmented and virtual reality applications that push boundaries and captivate users. ",
-			image: "/images/icons/service4.svg",
-		},
-		{
-			title: "Consultancy",
-			description:
-				"Gain valuable insights and expert guidance for your business where I personally analyze your chalanges, provide recommendations, and help you make inforemed decisions.",
-			image: "/images/icons/service6.svg",
-		},
-	],
+  title: "Our Services",
+  description: "",
+  services: [
+    {
+      title: "UI UX Design",
+      description:
+        "Transforming digital experiences with  UI/UX for mobile, websites, and web apps. Our expert team delivers pixel-perfect designs and seamless interactions that captivate users. Elevate your brand with our cutting-edge solutions.",
+      image: "/images/icons/logo-and-branding.svg",
+    },
+    {
+      title: "MVP Development",
+      description:
+        "Accelerate your product's journey to success with our MVP (Minimum Viable Product) development services. We specialize in rapidly prototyping and launching your idea, allowing you to test the market.",
+      image: "/images/icons/service4.svg",
+    },
+    {
+      title: "Biz Success Accelerator",
+      description:
+        "Establish Business Channel. Here, we'll create a stunning landing page, implement effective lead generation strategies, setup Google Ads campaigns, integrate Calendly, and establish Solid Base for you! ",
+      image: "/images/icons/service3.svg",
+    },
+
+    {
+      title: "Website Design and Development",
+      description:
+        "Unlock the full potential of your online business with our top-notch website development services. Our skilled team of developers will bring your vision to life, crafting a scalable website.",
+      image: "/images/icons/service2.svg",
+    },
+    {
+      title: "Mobile app Design and Development",
+      description:
+        "Revolutionize with our mobile app design and development services. Our expert team will create a captivating and user-friendly app, tailored to your goals. Book with us a meeting and let's explore!",
+      image: "/images/icons/service5.svg",
+    },
+    {
+      title: "Web app Design and Development",
+      description:
+        "Unleash your digital potential with our web app design and development services. Our team crafts stunning and functional web applications that elevate your business.",
+      image: "/images/icons/service6.svg",
+    },
+    {
+      title: "Game Development",
+      description:
+        "Level up your gaming experience with our exceptional game development services. Our talented team brings your ideas to life, creating immersive and captivating games that engage players.",
+      image: "/images/icons/service2.svg",
+    },
+    {
+      title: "AR VR Development",
+      description:
+        "Immerse your audience in extraordinary experiences with our AR/VR development services. Our team creates captivating augmented and virtual reality applications that push boundaries and captivate users. ",
+      image: "/images/icons/service4.svg",
+    },
+    {
+      title: "Consultancy",
+      description:
+        "Gain valuable insights and expert guidance for your business where I personally analyze your chalanges, provide recommendations, and help you make inforemed decisions.",
+      image: "/images/icons/service6.svg",
+    },
+  ],
 };
 
-
-
-
-
-
-
-
 export const projects = {
-	title: "Our Latest Creative Work",
-	projects: [
-		{
-			background: "bg-gradient1",
-			tags: ["Design", "Coding", "Development"],
-			features: [
-				"Development Strategy",
-				"UI/UX of the application",
-				"Backend Development",
-				"System Design and Deployment",
-			],
-			title: "Soarfare - Travel Startup Web App Development with Subscriptions",
-			desktopPreview: "/images/PortfolioPictures/Soarfare 640x410.png",
-			
-		},
-		{
-			background: "bg-gradient2",
-			tags: ["Design", "Coding", "Development"],
-			features: [
-				"Development Consultancy",
-				"UI/UX, Prototyping",
-				"Backend Development",
-				"Deployment and Testing",
-			],
-			title: "AJ Autos - MOT Garage Management System",
-			desktopPreview: "/images/PortfolioPictures/MOT 640X410.png",
-		},
-		{
-			background: "bg-gradient3",
-			tags: ["Design", "Mobile App", "Web App Development"],
-			features: [
-				"Design, Prototyping",
-				"Front End Development",
-				"Backend with Firebase",
-				"Extensive Testing",
-			],
-			title: "Jooba - Security Jobs Exploration Mobile and Web App",
-			desktopPreview: "/images/PortfolioPictures/jooba 640x410.png",
-		},
-		{
-			background: "bg-gradient4",
-			tags: ["Design", "Mobile App", "Web App Development"],
-			features: [
-				"Design, Prototyping",
-				"Front End Development",
-				"Backend with Firebase",
-				"Extensive Testing",
-			],
-			title: "WatchSwipe - Mobile App for Exploring High End Luxury Watches",
-			desktopPreview: "/images/PortfolioPictures/WatchSwipe 640x410.png",
-		},
-		{
-			background: "bg-gradient5",
-			tags: ["Design", "Development", "Deployment"],
-			features: [
-				"Wordpress Development",
-				"Custom Scripts Integration",
-				"Third Party Integrations",
-			],
-			title: "hypnosisfortraders.co.uk - Trading Psychology Learning Platform with Courses",
-			desktopPreview: "/images/PortfolioPictures/Hypnosis 640x410.png",
+  title: "Our Latest Creative Work",
+  projects: [
+    {
+      background: "bg-gradient5",
+      tags: ["Design", "Development", "Deployment"],
+      features: [
+        "Wordpress Development",
+        "Custom Scripts Integration",
+        "Third Party Integrations",
+      ],
+      title:
+        "hypnosisfortraders.co.uk - Trading Psychology Learning Platform with Courses",
+      desktopPreview: "/images/PortfolioPictures/Hypnosis 640x410.png",
+    },
 
+    {
+      background: "bg-gradient1",
+      tags: ["Design", "Coding", "Development"],
+      features: [
+        "Development Strategy",
+        "UI/UX of the application",
+        "Backend Development",
+        "System Design and Deployment",
+      ],
+      title: "Soarfare - Travel Startup Web App Development with Subscriptions",
+      desktopPreview: "/images/PortfolioPictures/Soarfare 640x410.png",
+    },
+	{
+		background: "bg-gradient8",
+		tags: ["Design", "Development", "Maintenance"],
+		features: [
+		  "Wordpress Development",
+		  "Custom Backend",
+		  "Plugins Integration",
+		],
+		title: "Minerals Mania Gems -  Wordpress Store with Inventory Management and Customizations ",
+		desktopPreview: "/images/PortfolioPictures/minerals mania 640x410.png",
+	  },
+    {
+      background: "bg-gradient4",
+      tags: ["Design", "Development", "Deployment"],
+      features: [
+        "Wordpress Development",
+        "Custom Scripts Integration",
+        "Third Party Integrations",
+      ],
+      title: "Rayan Taxi - Web Platform for Booking Private Cabs",
+      desktopPreview: "/images/PortfolioPictures/Rayan Taxi 640x410.png",
+    },
+	{
+		background: "bg-gradient3",
+		tags: ["HTML", "CSS", "Php/Laravel"],
+		features: [
+		  "Backend Development",
+		  "Html/Css Frontend",
+		  "SEO Sptimisation",
+		],
+		title: "The Khorasan Diary -  System for Articles Publishing, Blogs Management and Media Strategy",
+		desktopPreview: "/images/PortfolioPictures/The Khorasan Diary 640x410.png",
+	  },
 
-		},
-
-		{
-			background: "bg-gradient6",
-			tags: ["Design", "Development", "Deployment"],
-			features: [
-				"Wordpress Development",
-				"Custom Scripts Integration",
-				"Third Party Integrations",
-			],
-			title: "Rayan Taxi - Web Platform for Booking Private Cabs",
-			desktopPreview: "/images/PortfolioPictures/Rayan Taxi 640x410.png",
-
-
-		}
-	],
+    {
+      background: "bg-gradient2",
+      tags: ["Design", "Coding", "Development"],
+      features: [
+        "Development Consultancy",
+        "UI/UX, Prototyping",
+        "Backend Development",
+        "Deployment and Testing",
+      ],
+      title: "AJ Autos - MOT Garage Management System",
+      desktopPreview: "/images/PortfolioPictures/MOT 640X410.png",
+    },
+    {
+      background: "bg-gradient3",
+      tags: ["Design", "Mobile App", "Web App Development"],
+      features: [
+        "Design, Prototyping",
+        "Front End Development",
+        "Backend with Firebase",
+        "Extensive Testing",
+      ],
+      title: "Jooba - Security Jobs Exploration Mobile and Web App",
+      desktopPreview: "/images/PortfolioPictures/jooba 640x410.png",
+    },
+    {
+      background: "bg-gradient4",
+      tags: ["Design", "Mobile App", "Web App Development"],
+      features: [
+        "Design, Prototyping",
+        "Front End Development",
+        "Backend with Firebase",
+        "Extensive Testing",
+      ],
+      title: "WatchSwipe - Mobile App for Exploring High End Luxury Watches",
+      desktopPreview: "/images/PortfolioPictures/WatchSwipe 640x410.png",
+    },
+  ],
 };
 
 export const skills = {
-	intro: "",
-	title: "Industries We Serve",
-	description:
-		"",
-	skills: [
-		{ name: "Angular", icon: "devicon:angular" },
-		{ name: "AWS", icon: "logos:aws" },
-		{ name: "SQL", icon: "vscode-icons:file-type-sql" },
-		{ name: "Django", icon: "vscode-icons:file-type-django" },
-		{ name: "Docker", icon: "logos:docker-icon" },
-		{ name: "Express", icon: "devicon:express-wordmark" },
-		{ name: "Firebase", icon: "vscode-icons:file-type-firebase" },
-		{ name: "Flutter", icon: "devicon:flutter" },
-		{ name: "GCP", icon: "skill-icons:gcp-light" },
-		{ name: "Java", icon: "logos:java" },
-		{ name: "Kubernetes", icon: "logos:kubernetes" },
-		{ name: "Kotlin", icon: "logos:kotlin-icon" },
-		{ name: "MongoDB", icon: "vscode-icons:file-type-mongo" },
-		{ name: "React Native", icon: "logos:react" },
-		{ name: "Node", icon: "vscode-icons:file-type-node" },
-		{ name: "React", icon: "logos:react" },
-		{ name: "Swift", icon: "logos:swift" },
-		{ name: "Vue", icon: "logos:vue" },
-		{ name: "Wordpress", icon: "skill-icons:wordpress" },
-		{ name: "Wix", icon: "logos:wix" },
-		{ name: "Shopify", icon: "logos:shopify" },
-		{ name: "Webflow", icon: "devicon:webflow" },
-		{ name: "Square Space", icon: "jam:squarespace-square" },
-	],
+  intro: "",
+  title: "Industries We Serve",
+  description: "",
+  skills: [
+    { name: "Angular", icon: "devicon:angular" },
+    { name: "AWS", icon: "logos:aws" },
+    { name: "SQL", icon: "vscode-icons:file-type-sql" },
+    { name: "Django", icon: "vscode-icons:file-type-django" },
+    { name: "Docker", icon: "logos:docker-icon" },
+    { name: "Express", icon: "devicon:express-wordmark" },
+    { name: "Firebase", icon: "vscode-icons:file-type-firebase" },
+    { name: "Flutter", icon: "devicon:flutter" },
+    { name: "GCP", icon: "skill-icons:gcp-light" },
+    { name: "Java", icon: "logos:java" },
+    { name: "Kubernetes", icon: "logos:kubernetes" },
+    { name: "Kotlin", icon: "logos:kotlin-icon" },
+    { name: "MongoDB", icon: "vscode-icons:file-type-mongo" },
+    { name: "React Native", icon: "logos:react" },
+    { name: "Node", icon: "vscode-icons:file-type-node" },
+    { name: "React", icon: "logos:react" },
+    { name: "Swift", icon: "logos:swift" },
+    { name: "Vue", icon: "logos:vue" },
+    { name: "Wordpress", icon: "skill-icons:wordpress" },
+    { name: "Wix", icon: "logos:wix" },
+    { name: "Shopify", icon: "logos:shopify" },
+    { name: "Webflow", icon: "devicon:webflow" },
+    { name: "Square Space", icon: "jam:squarespace-square" },
+  ],
 };
 
 export const teamMembers = {
-	intro: "We Are Awesome",
-	title: "Our Team Members",
-	description:
-		"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-	members: [
-		
-		{
-			name: "Talal",
-			role: "Backend Developer, Database Management, Server Side Configurations and AWS",
-			image: "/images/team/Talal 3.png",
-		},
+  intro: "We Are Awesome",
+  title: "Our Team Members",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  members: [
+    {
+      name: "Talal",
+      role: "Backend Developer, Database Management, Server Side Configurations and AWS",
+      image: "/images/team/Talal 3.png",
+    },
 
-		{
-			name: "Ibrahim",
-			role: "Database Architect, UX, System Design and Front End",
-			image: "/images/team/Ibrahim 1.png",
-		},
+    {
+      name: "Ibrahim",
+      role: "Database Architect, UX, System Design and Front End",
+      image: "/images/team/Ibrahim 1.png",
+    },
 
-		{
-			name: "Taymor",
-			role: "Php Laravel, Custom Scripts, Firebase, Dev-ops",
-			image: "/images/team/Ahsan 1.png",
-		},
-
-
-
-	]
+    {
+      name: "Taymor",
+      role: "Php Laravel, Custom Scripts, Firebase, Dev-ops",
+      image: "/images/team/Ahsan 1.png",
+    },
+  ],
 };
 
 export const footer = {
-	title: "WE WOULD LOVE TO HEAR FROM YOU.",
-	subtitle:
-		"Feel free to reach out if you want to collaborate with us, or simply have a call.",
-	contact: {
-		email: "contact@bostonpixel.com",
-		phone: "+923351066628",
-	},
-	addresses: [
-		{
-			area: "Boston",
-			address: "Boston, Massachusetts.",
-		},
-		{
-			area: "Islamabad",
-			address: "Block 3, I-10 Markaz, Islamabad, PK 44000",
-		},
-	],
+  title: "WE WOULD LOVE TO HEAR FROM YOU.",
+  subtitle:
+    "Feel free to reach out if you want to collaborate with us, or simply have a call.",
+  contact: {
+    email: "contact@bostonpixel.com",
+    phone: "+923351066628",
+  },
+  addresses: [
+    {
+      area: "Boston",
+      address: "Boston, Massachusetts.",
+    },
+    {
+      area: "Islamabad",
+      address: "Block 3, I-10 Markaz, Islamabad, PK 44000",
+    },
+  ],
 };
